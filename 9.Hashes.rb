0cc7e9954bbe70cc0da885c1fb3fed83b0ed38d6
Enzo <enzogambrosio@hotmail.com>
2025-01-11 18:45:49 -0300

    My study session for today
    Now starting to make i tica-tac-toe game again

---
+++ 9.Hashes.rb
@@ -0,0 +1,11 @@
+#List of a key and values paired.
+states = {
+    #Key            =   value
+    "Pennshylvania" => "PA",
+    "New York"  => "NY",
+    "Oregon" => "OR", 
+    :Miami => "MI"
+}
+
+puts states["Oregon"]
+puts states[:Miami]
